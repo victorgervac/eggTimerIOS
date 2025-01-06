6cbf7a8c71428f45b2d2cd8db6d5053ece8aeddb
victorgervac <victorgervacio33@gmail.com>
2025-01-06 09:28:48 -0700

    sounds on alarm?

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,10 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     let eggTimes = ["Soft": 10, "Medium": 420, "Hard": 720]
-
+    var player: AVAudioPlayer?
     var timer = Timer()
     var totalTime: Int = 0
     var secondsPass: Int = 0
@@ -28,6 +29,10 @@ class ViewController: UIViewController {
         let hardness = sender.currentTitle!
 
         totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPass = 0
+        ChangeLabel?.text = hardness
 
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
@@ -37,15 +42,25 @@ class ViewController: UIViewController {
     @objc func updateCounter() {
         //example functionality
         if secondsPass < totalTime {
-          let percentage = Double(secondsPass)/Double(totalTime)
+          let percentage = Float(secondsPass)/Float(totalTime)
             print(percentage)
             secondsPass += 1
             progressBar.progress = Float(percentage)
         
         }else{
             timer.invalidate()
-            ChangeLabel.text = "done"
+            ChangeLabel.text = "done!"
+            playSound()
         }
+   
+                        
+    }
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension:"mp3")
+       
+        player = try! AVAudioPlayer(contentsOf: url! )
+        player.play()
+
     }
     
 
