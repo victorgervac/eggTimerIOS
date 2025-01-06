d5a43865953860482c84cc75b6dfa716780fd9c3
victorgervac <victorgervacio33@gmail.com>
2025-01-06 15:08:56 -0700

    play sound when timer hit 9.99

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -59,7 +59,7 @@ class ViewController: UIViewController {
         let url = Bundle.main.url(forResource: "alarm_sound", withExtension:"mp3")
        
         player = try! AVAudioPlayer(contentsOf: url! )
-        player.play()
+        player!.play()
 
     }
     
